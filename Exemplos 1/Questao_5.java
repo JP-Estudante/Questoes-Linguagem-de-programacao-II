cfd0dada309c738bee2e5e3ce116e15d486e8628
João Pedro Scheffler <joaopedroscheffler@gmail.com>
2023-04-08 18:14:10 -0300

    Sync

--- Exemplos 1/Questao_5.java
+++ Exemplos 1/Questao_5.java
@@ -1,30 +1,33 @@
-//INCOMPLETA
 import java.util.Scanner;
 
 public class Questao_5 {
     public static void main(String[] args) {
-        double tmpVolta, menorTmp, somaTmp; //TMP == Tempo
-        int numVoltas, voltaMenTmp, i; //voltaMenTmp == VoltaMenorTempo
-    
+        double tmpVolta, menorTmp = Double.MAX_VALUE, somaTmp = 0; // TMP == Tempo
+        int numVoltas, voltaMenTmp = 1, i; // voltaMenTmp == VoltaMenorTempo
+
         Scanner teclado = new Scanner(System.in);
 
         System.out.print("Quantidade de voltas: ");
         numVoltas = teclado.nextInt();
 
-        for(i = 0; i < numVoltas; i++){
-            System.out.print("Tempo da "+ (i+1) +"º volta: ");
-            System.out.println();
+        try {
+            for (i = 0; i < numVoltas; i++) {
+                System.out.print("Tempo da " + (i + 1) + "º volta: ");
+                tmpVolta = teclado.nextDouble();
 
-            tmpVolta = teclado.nextDouble();
-            menorTmp = tmpVolta;
-            voltaMenTmp = 1;
-            somaTmp = tmpVolta;
+                if (tmpVolta < menorTmp) {
+                    menorTmp = tmpVolta;
+                    voltaMenTmp = i + 1;
+                }
 
-            if(tmpVolta < menorTmp){
-                menorTmp = tmpVolta;
-                voltaMenTmp = i+1;
+                somaTmp += tmpVolta;
             }
+        } finally {
+            teclado.close();
         }
-        teclado.close();
+
+        System.out.println("Menor tempo: " + menorTmp);
+        System.out.println("Volta do menor tempo: " + voltaMenTmp);
+        System.out.println("Tempo médio: " + somaTmp / numVoltas);
     }
 }
