d47ddb80b63ab9c83dd15fe01ba3c5b140e62a9c
João Pedro Scheffler <joaopedroscheffler@gmail.com>
2023-05-10 23:57:41 -0300

    Sync

--- sistema_bancario/src/sistema_bancario/Principal.java
+++
@@ -1,8 +0,0 @@
-package sistema_bancario;
-
-public class Principal {   
-    public static void main(String[] args) {
-        JanelaPrincipal janela = new JanelaPrincipal();
-        janela.setVisible(true);        
-    }    
-}
